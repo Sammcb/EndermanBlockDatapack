f0ba5949c016832fd2dd41f70eeddb8de1c8c07e
Samuel McBroom <sam@sammcb.com>
2023-01-20 08:22:16 -0800

    Initial release.

---
+++ do.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+VERSION=1.0.0
+MINECRAFT_VERSION=1.19.3
+
+ZIP_NAME="enderman-block-${VERSION}-${MINECRAFT_VERSION}.zip"
+
+print_info() {
+	printf "\e[1;35m$1\e[0m - \e[0;37m$2\e[0m\n"
+}
+
+help() {
+	print_info help "Display callable targets"
+	print_info build "Create a datapack zip file"
+	print_info clean "Remove build artifacts"
+}
+
+build() {
+	clean
+	zip -q -r ${ZIP_NAME} data pack.mcmeta pack.png
+}
+
+clean() {
+	rm -f ${ZIP_NAME}
+}
+
+if [ ${1:+x} ]; then
+	$1
+else
+	help
+fi
+	
