350126f0dbaaf650394e9c4a7d176445f0d4e257
Samuel McBroom <sam@sammcb.com>
2024-12-23 09:25:19 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -63,22 +63,22 @@
 					dontBuild = true;
 					doCheck = true;
 					nativeBuildInputs = with pkgs; [zizmor];
-					checkPhase = "zizmor -n .";
+					checkPhase = "zizmor -q .";
 					installPhase = "touch $out";
 				};
 
 				# This takes a long time to install in a runner
 				# Disabling till nix hopefully allows running individual checks
 
-				# flake = pkgs.stdenvNoCC.mkDerivation {
-				# 	name = "flake-checker";
-				# 	src = self;
-				# 	dontBuild = true;
-				# 	doCheck = true;
-				# 	nativeBuildInputs = [flakeCheckerPackage];
-				# 	checkPhase = "flake-checker --no-telemetry";
-				# 	installPhase = "touch $out";
-				# };
+				flake = pkgs.stdenvNoCC.mkDerivation {
+					name = "flake-checker";
+					src = self;
+					dontBuild = true;
+					doCheck = true;
+					nativeBuildInputs = [flakeCheckerPackage];
+					checkPhase = "flake-checker --no-telemetry";
+					installPhase = "touch $out";
+				};
 			};
 
 			packages = {
