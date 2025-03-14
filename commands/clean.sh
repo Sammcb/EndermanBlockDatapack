5ce78fa18129a473571985a9f4f79c554532cd53
Samuel McBroom <sam@sammcb.com>
2025-03-14 10:31:20 -0700

    Update build process.

--- commands/clean.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -d "${build_directory}" ]; then
-	echo "Build directory '${build_directory}' not found, skipping clean..."
-	exit 0
-fi
-
-echo "Deleting build directory '${build_directory}'..."
-
-rm -r "${build_directory}"
